7657ec456f2924da1b890d97551ef8c77993b586
David Rose <doppler@gmail.com>
2020-12-16 14:35:07 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const size = 1000;
+
+  const phi = 1 / (144 / 89);
+  const center = size / 2;
+  const r1 = size / 2;
+  const r2 = r1 * phi;
+  const r3 = r1 * phi**2;
+
+  const pentaAngles = Array.from(Array(10)).map((_, i) => i * (360 / 10))
+  const quintaAngles = Array.from(Array(30)).map((_, i) => i * (360 / 30))
+
+  const polarCoords = (angle: number, radius: number) => {
+    return {
+      x: center + radius * Math.cos((angle - 90) * (Math.PI / 180)),
+      y: center + radius * Math.sin((angle - 90) * (Math.PI / 180))
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${size} ${size}`}>
+        {[r1, r2, r3].map(r => (
+          <circle className='transparent stroked' 
+            cx={center} cy={center} r={r} 
+          />
+        ))}
+
+        {quintaAngles.map(a => (
+          <g>
+            <line className='stroked dim' x1={polarCoords(a, r3).x} y1={polarCoords(a, r3).y} x2={polarCoords(a, r1).x} y2={polarCoords(a, r1).y} />
+            <text x={polarCoords(a, r1).x} y={polarCoords(a, r1).y}>{a}</text>
+          </g>
+        ))}
+
+        <path className='transparent stroked'
+          d={pentaAngles.map((a, i) => `
+            ${i === 0 ? 'M ' : 'L '} 
+            ${polarCoords(a, a % 72 === 0 ? r1 : r3).x} 
+            ${polarCoords(a, a % 72 === 0 ? r1 : r3).y} 
+          `).join(' ') + ' Z' }
+        />
+      </svg>
+      {/* <code>{JSON.stringify({phi, center, r1, r2, r3, pentaAngles}, null, 2)}</code> */}
     </div>
   );
 }
